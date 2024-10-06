16d966b73e99868511a6a908ecab36d4f428de93
Earl Tacogdoy <laternoobs@gmail.com>
2024-10-06 15:07:01 +0800

    #4 - October 6, 2024
    - Added Unit Test for fetchTasks
    - Removed unnecessary testing
    - Added doc-comments for Tasks Model
    - Added fetchTasks, listed and shown tasks
    - Added github action workflow. #17
    - Added testing .env

--- app/Livewire/Tasks.php
+++ app/Livewire/Tasks.php
@@ -2,12 +2,31 @@
 
 namespace App\Livewire;
 
+use App\Models\Tasks as ModelsTasks;
 use Livewire\Component;
 
 class Tasks extends Component
 {
+    private $tasks = [];
+    public $task;
+
+    private function getTasks()
+    {
+        $model = new ModelsTasks();
+        $this->tasks = $model->fetchTasks();
+    }
+
+    public function setTask(ModelsTasks $task)
+    {
+        $this->task = $task;
+    }
+
     public function render()
     {
-        return view('livewire.tasks');
+        $this->getTasks();
+        return view('livewire.tasks', [
+            'tasks' => $this->tasks,
+            'task' => $this->task,
+        ]);
     }
 }
--- resources/views/livewire/tasks.blade.php
+++ resources/views/livewire/tasks.blade.php
@@ -1,3 +1,22 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    <ul>
+        @foreach ($tasks as $t)
+            <li><button wire:click="setTask({{ $t->id }})">{{ $t->title }}</button></li>
+        @endforeach
+    </ul>
+
+    {{-- Like modal for tasks view --}}
+    @if ($task)
+        <div>
+            <p>{{ $task->title }}</p>
+            <p>{{ $task->description }}</p>
+            <p>
+                @if ($task->is_done)
+                    <input type="checkbox" checked value="Done">
+                @else
+                    <input type="checkbox" value="Done">
+                @endif
+            </p>
+        </div>
+    @endif
 </div>
